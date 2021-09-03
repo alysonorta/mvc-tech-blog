b28231a75f8802e116a86f27a6f4512be4eadf33
Alyson Orta <alyson.orta@gmail.com>
2021-09-03 18:44:56 -0400

    setting up server file

---
+++ server.js
@@ -0,0 +1,39 @@
+//Dependencies
+const routes = require ('./Develop/controllers')
+const path = require('path');
+
+//Express dependencies
+const express = require ('express');
+const exphbs = require ('express-handlebars');
+const hbs = exphbs.create({});
+const session = require ('express-session');
+
+//Sequelize dependencies
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Secret message here...',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on port:' + PORT));
+});
