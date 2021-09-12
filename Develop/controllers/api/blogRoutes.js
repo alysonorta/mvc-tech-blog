c44ead7db7ef318ed8348ba0aaba9168281d934f
Alyson Orta <alyson.orta@gmail.com>
2021-09-12 12:06:36 -0400

    added content to handlebars files and updated file routes

--- Develop/controllers/api/blogRoutes.js
+++ Develop/controllers/api/blogRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Blog } = require ('../../models');
-const withAuth = require ('../../../utils/auth');
+const withAuth = require ('../../utils/auth');
 
 
 //adding ability to create a new blog post if the user is logged in
--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require ('express').Router();
 const { User, Blog } = require ('../models');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../utils/auth');
 
 //Get all projects to then join with user data
 router.get('/', async (req, res) => {
